dbac47db3e62acf664b5bec75db8c33fcb658f22
mar-quitox <marcosdjuarezs@gmail.com>
2015-09-25 12:38:47 -0600

    Instalación entorno migraciones

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+	if ($this->app->environment() == 'local') {
+        $this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
+    }
         //
     }
 }
