043564ac2592338eb753f6376f888ca425313e1f
IsaacCR <isaaccivan@gmail.com>
2015-09-30 20:00:21 -0600

    Creación de modelos para el modulo de cafeteria

---
+++ database/migrations/2015_10_01_015136_create_Temporada_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTemporadaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Temporada', function(Blueprint $table) {
+            $table->increments('id');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('Temporada');
+    }
+}
---
+++ database/migrations/2015_10_01_015549_create_DetalleCompra_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDetalleCompraTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('DetalleCompra', function(Blueprint $table) {
+            $table->increments('id');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('DetalleCompra');
+    }
+}
