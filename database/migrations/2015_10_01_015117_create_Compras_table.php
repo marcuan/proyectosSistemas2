fc8071fbc47d0515296f5160ae115ab1c2895468
guinac <guinac_14@hotmail.com>
2015-10-06 23:55:59 -0600

    Atributos a compras

--- database/migrations/2015_10_01_015117_create_Compras_table.php
+++ database/migrations/2015_10_01_015117_create_Compras_table.php
@@ -14,7 +14,14 @@ class CreateComprasTable extends Migration
     {
         Schema::create('Compras', function(Blueprint $table) {
             $table->increments('id');
-            
+            $table->datetime('fecha');
+            $table->float('subTotal');
+            $table->float('descuento');
+            $table->float('total');
+            $table->integer('cliente_id')->unsigned();
+            $table->foreign('cliente_id')->references('id')->on('Cliente');
+            $table->integer('empleado_id')->unsigned();
+            $table->foreign('empleado_id')->references('id')->on('Empleado');
             $table->timestamps();
         });
     }
