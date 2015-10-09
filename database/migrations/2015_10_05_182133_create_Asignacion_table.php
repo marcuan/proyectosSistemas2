a0fd5995aac02995da4b7cbd5de7153adc798e4e
mar-quitox <marcosdjuarezs@gmail.com>
2015-10-09 10:57:11 -0600

    Arreglo referencia de estudiantes

--- database/migrations/2015_10_05_182133_create_Asignacion_table.php
+++ database/migrations/2015_10_05_182133_create_Asignacion_table.php
@@ -15,8 +15,8 @@ class CreateAsignacionTable extends Migration
         Schema::create('Asignacion', function(Blueprint $table) {
             $table->increments('id');
             $table->string('nombre');
-            $table->integer('id_estudiante');
-            $table->foreign('id_estudiante')->reference('id')->on('Estudiante');
+            $table->integer('id_estudiantes');
+            $table->foreign('id_estudiantes')->reference('id')->on('Estudiantes');
             $table->timestamps();            
         });
     }
