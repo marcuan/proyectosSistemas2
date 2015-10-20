9e2f96bb9c0beb1dfc46d473d466f9846814eb7a
IsaacCR <isaaccivan@gmail.com>
2015-10-20 17:50:02 -0600

    Mejorando vista de materia prima

--- resources/views/materiaprima/todamateriaprima.blade.php
+++ resources/views/materiaprima/todamateriaprima.blade.php
@@ -1,16 +1,28 @@
 @extends('layouts.master')
+<?php $message=Session::get('message')?>
 
-@section('title', 'Page Title')
-
-@section('sidebar')
-    @parent
-
-    <p>This is appended to the master sidebar.</p>
-@endsection
-
+@if($message == 'store')
+<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  <strong>Creado</strong> Se ha creado nueva materia prima.
+</div>
+@endif
 @section('content')
-    <p>Materia Prima Disponible</p>
-    @foreach($materiaprima as $dato)
-        <p>{{ $dato->nombre }}</p>
-    @endforeach
-@endsection
+    <div class="container">
+        <table class="table">
+            <thead>
+                <th>Nombre</th>
+                <th>Existencia</th>
+                <th>Precio</th>
+                
+            </thead>
+             @foreach($materiaprima as $dato)
+            <tbody>
+                <td>{{$dato->nombre}}</td>
+                <td>{{$dato->existencia}}</td>
+                <td>{{$dato->precio}}</td>
+                 </tbody>
+            @endforeach
+        </table>
+    </div>
+@stop
