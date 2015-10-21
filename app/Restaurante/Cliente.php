17b1401c200a193b82edffc2a641b6f530175e07
EstuardoTS <stuardih@gmail.com>
2015-10-20 23:41:50 -0600

    Difiniendo tabla clientes y sus atributos, en Clase Clientes

--- app/Restaurante/Cliente.php
+++ app/Restaurante/Cliente.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     //
+    protected $table = 'Clientes';
+
+    protected $fillable = [
+    'nombre',
+    'nit',
+    'telefono',
+    'dirección'];
 }
