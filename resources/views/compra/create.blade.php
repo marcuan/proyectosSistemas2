fc3ef0c2d6b51fb0551bdb63c35979c97024a083
Gerson1villa <gerson1vime@gmail.com>
2015-10-23 12:16:48 -0600

    front restaurante

--- resources/views/compra/create.blade.php
+++ resources/views/compra/create.blade.php
@@ -2,28 +2,30 @@
 @section('content')
     {!!Form::open(['route'=>'compra.store', 'method'=>'POST'])!!}
         <h3>Compras</h3>
-        <div class="container">
+        <div class="container col-xs-12">
             <div class="form-grup">
         		{!!Form::label('Fecha:')!!}
         		{!!Form::date('fecha',null,['class'=>'form-control','placeholder'=>'Ingrese fecha', 'required'])!!}
-        	</div>
+        	</div><br>
         	<div class="form-grup">
         		{!!Form::label('Sub Total:')!!}
         		{!!Form::text('subTotal',null,['class'=>'form-control','placeholder'=>'Ingrese Sub Total', 'required'])!!}
-        	</div>
+        	</div><br>
         	<div class="form-grup">
         		{!!Form::label('Descuento:')!!}
         		{!!Form::text('descuento',null,['class'=>'form-control','placeholder'=>'Ingrese Descuento', 'required'])!!}
-        	</div>
+        	</div><br>
             <div class="form-grup">
                 {!!Form::label('Total:')!!}
                 {!!Form::text('total',null,['class'=>'form-control','placeholder'=>'Ingrese Total', 'required'])!!}
-            </div>
+            </div><br>
             <div class="form-grup">
                 {!!Form::label('Proveedor:')!!}
                 {!!Form::text('proveedores_id',null,['class'=>'form-control','placeholder'=>'Ingrese Proveedor', 'required'])!!}
-            </div>
+            </div><br>
+            <div class="form-btn">
         	{!!Form::submit('Guardar',['class'=>'btn btn-primary'])!!}
+            </div>
         </div>
     {!!form::close()!!} 
 @stop
--- resources/views/materiaprima/edit.blade.php
+++ resources/views/materiaprima/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
   {!!Form::model($materiaprima,['route'=>['materiaprima.update', $materiaprima->id], 'method'=>'PUT'])!!}
         <h3>Materia Prima</h3>
-       <div class="container">
+       <div class="container col-xs-12">
             <div class="form-grup">
                 {!!Form::label('Nombre:')!!}
                 {!!Form::text('nombre',null,['class'=>'form-control','placeholder'=>'Ingrese Nombre de Materia ','required'])!!}
@@ -17,8 +17,9 @@
                 {!!Form::number('precio',null,['class'=>'form-control','placeholder'=>'Ingrese precio','required','step' => 'any'])!!}
             </div>
       
-            
+            <div class="form-btn">
             {!!Form::submit('Registrar',['class'=>'btn btn-primary'])!!}
+            </div>
          </div>
     {!!form::close()!!}
 
