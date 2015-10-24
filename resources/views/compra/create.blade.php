555b2d924cb492e9da817e61b9c8006e2c2b30b4
guinac <guinac_14@hotmail.com>
2015-10-23 22:18:15 -0600

    Editar compras

--- resources/views/compra/create.blade.php
+++ resources/views/compra/create.blade.php
@@ -21,7 +21,7 @@
             </div>
             <div class="form-grup">
                 {!!Form::label('Proveedor:')!!}
-                {!!Form::text('proveedores_id',null,['class'=>'form-control','placeholder'=>'Ingrese Proveedor', 'required'])!!}
+                {!!Form::select('proveedores_id', $opcionproveedor,['class'=>'form-control','placeholder'=>'Ingrese Proveedor', 'required'])!!}
             </div>
         	{!!Form::submit('Guardar',['class'=>'btn btn-primary'])!!}
         </div>
--- resources/views/compra/index.blade.php
+++ resources/views/compra/index.blade.php
@@ -17,7 +17,7 @@
 @endif
 
 @section('content')
-    <a href="/proyectosSistemas2/public/compra/create" class="btn btn-danger">Crear Compra</a>
+    <a href="compra/create" class="btn btn-danger">Crear Compra</a>
     <div class="container">
         <table class="table">
             <thead>
@@ -27,20 +27,16 @@
                 <th>Total</th>
                 <th>Proveedor</th>
             </thead>
-
-            @foreach($compra as $compras)
-            <tbody>
-                <td>{{$compras->fecha}}</td>
-                <td>{{$compras->subTotal}}</td>
-                <td>{{$compras->descuento}}</td>
-                <td>{{$compras->total}}</td>
-                <td>{{$compras->proveedores_id}}</td>
-                <td>{!!link_to_route('compra.edit', $title = 'Editar', $parameters = $compras->id, $attributes = ['class'=>'btn btn-primary']);!!}</td>
-            </tbody>
+            @foreach($compra as $compra)
+                <tbody>
+                    <td>{{$compra->fecha}}</td>
+                    <td>{{$compra->subTotal}}</td>
+                    <td>{{$compra->descuento}}</td>
+                    <td>{{$compra->total}}</td>
+                    <td>{{$compra->proveedores_id}}</td>
+                    <td>{!!link_to_route('compra.edit', $title = 'Editar', $parameters = $compra->id, $attributes = ['class'=>'btn btn-primary']);!!}</td>
+                </tbody>
             @endforeach
-
-
-
         </table>
     </div>
 @stop
