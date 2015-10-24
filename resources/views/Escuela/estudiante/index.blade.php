137002e57e4221440d40ee11123e9d8832e7c75a
Marco Barrios <mabarriosmarco@gmail.com>
2015-10-23 21:17:01 -0600

    Se añadió boton para ver cursos asignados

--- resources/views/Escuela/estudiante/index.blade.php
+++ resources/views/Escuela/estudiante/index.blade.php
@@ -41,7 +41,8 @@
                 <td>{{$estudiante->direccion}}</td>
                 <td>{{$estudiante->correo}}</td>
                 <td>{!!link_to_route('estudiantes.edit', $title = 'Editar', $parameters = $estudiante->id, $attributes = ['class'=>'btn btn-primary']);!!}
-                    {!!link_to('asignacionestudiantes/'.$estudiante->id, $title = 'Asignar Cursos', $attributes = ['class'=>'btn btn-success'], $secure = null);!!}</td>
+                    {!!link_to('asignacionestudiantes/'.$estudiante->id, $title = 'Asignar Cursos', $attributes = ['class'=>'btn btn-success'], $secure = null);!!}
+                    {!!link_to_route('estudiantes.show', $title = 'Ver Cursos', $parameters = $estudiante->id, $attributes = ['class'=>'btn btn-warning']);!!}</td>
             </tbody>
             @endforeach
         </table>
--- resources/views/Escuela/maestro/assign.blade.php
+++ resources/views/Escuela/maestro/assign.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.principal')
 @section('content')
     <div class="container">
+    <a href="../maestros" class="btn btn-danger">Regresar</a>
         <table class="table">
             <thead>
                 <th>Codigo</th>
@@ -9,7 +10,6 @@
                 <th>Fecha de Inicio</th>
                 <th>Fecha de Finalizacion</th>
                 <th>Maximo de Estudiantes</th>
-                <th>Operación</th>
             </thead>
             @foreach($course as $curso)
             <tbody>
@@ -19,7 +19,6 @@
                 <td>{{$curso->fecha_inicio}}</td>
                 <td>{{$curso->fecha_fin}}</td>
                 <td>{{$curso->max_estudiantes}}</td>
-                <td>{!!link_to_route('cursos.edit', $title = 'Asignar', $parameters = $curso->id, $attributes = ['class'=>'btn btn-primary']);!!}</td>
             </tbody>
             @endforeach
         </table>
