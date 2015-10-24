ae5e0cc5df1f5bba4eb4faedb0a113c3f41fa1b3
Angel Velasquez <jose.velasquez311@gmail.com>
2015-10-24 17:34:53 -0600

    Arreglando
    he arreglado unas cosillas

--- app/Http/Controllers/Despensa/VentasController.php
+++ app/Http/Controllers/Despensa/VentasController.php
@@ -16,7 +16,9 @@ class VentasController extends Controller
     }
     public function crear ()
     {
-        return 'hola miriam te extraño demasiado';
+        $ventas = new App\Cliente;
+        $ventas -> clientes_id = '1';
+        $ventas -> save();
     }
     public function lista ()
     {
