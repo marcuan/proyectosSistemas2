ae6d8ce3ab593a7d0db9acd6001e20b8e9c49a1c
georeliz <ga_gdl@outlook.com>
2015-10-24 21:52:32 -0600

    Ajustes proveedores

--- resources/views/proveedores/edit.blade.php
+++ resources/views/proveedores/edit.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.principal')
 @section('content')
         
-    {!!Form::model($proveedor,['route'=>['proveedores.update', $proveedor->id], 'method'=>'PUT'])!!}
+    {!!Form::model($proveedores,['route'=>['proveedores.update', $proveedores->id], 'method'=>'PUT'])!!}
         <h3>Proveedores</h3>
         <div class="container col-xs-12">
             <div class="form-grup">
                 {!!Form::label('Proveedor:')!!}
-                {!!Form::text('nombre_proveedor',null,['class'=>'form-control','placeholder'=>'Ingrese Nombre del Proveedor', 'required'])!!}
+                {!!Form::text('nombre',null,['class'=>'form-control','placeholder'=>'Ingrese Nombre del Proveedor', 'required'])!!}
             </div><br>
             <div class="form-grup">
                 {!!Form::label('Telefono:')!!}
