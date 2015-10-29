024f531b8497ac1e04e40450889d6cb84a2c4b47
EstuardoTS <stuardih@gmail.com>
2015-10-28 23:37:41 -0600

    Cambio en paginacion clientes, y creacion de la rama

--- app/Http/Controllers/Restaurante/ClienteController.php
+++ app/Http/Controllers/Restaurante/ClienteController.php
@@ -21,7 +21,7 @@ class ClienteController extends Controller
     {
         //
         //$customer = Cliente::All();
-        $customer = Cliente::name($request->get('name'))->orderBy('nombre', 'DESC')->paginate();
+        $customer = Cliente::name($request->get('name'))->orderBy('nombre', 'DESC')->paginate(7);
         return view ('cliente.index',compact('customer'));
     }
 
