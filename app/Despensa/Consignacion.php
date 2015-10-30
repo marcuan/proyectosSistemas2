eacbcc971521cc4918d604e6618dbf3346680abb
angelTibu <jose.velasquez311@gmail.com>
2015-10-30 12:59:39 -0600

    Update Consignacion.php

--- app/Despensa/Consignacion.php
+++ app/Despensa/Consignacion.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Consignacion extends Model
 {
     protected $table = 'Consignacion';
-
     protected $fillable = [
     	'fechaInicial',
     	'fechaFinal',
