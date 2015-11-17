b5ac94c387a490280573f20d67a352b1bc87d279
JuantaMajose <majoyuanlow@gmail.com>
2015-11-16 23:53:59 -0600

    ultima modificación a  detalle de consignación

--- resources/views/detalleconsignacion/create.blade.php
+++ resources/views/detalleconsignacion/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.dinamic')
+@extends('layouts.principal')
 <?php $message=Session::get('message')?>
 
 @if($message == 'create')
@@ -24,13 +24,13 @@
 
 		
 			 {!! Form::label('Seleccione un Producto: ')!!}
+		 
 		     {!! Form::select('producto_id',$opcionproducto);!!}
 		
-			 {!! Form::number('precio',null,['class'=>'form-control','placeholder'=>'Ingrese Precio','required']); !!}
 	   <div class="form-grup"> 
 			 <div class="form-grup">
 	         {!!Form::label('Cantidad:')!!}
-             {!!Form::number('cantidad',null,['class'=>'form-control','placeholder'=>'Ingrese Cantidad','required'])!!}
+			 {!!Form::number('cantidad',null,['class'=>'form-control','placeholder'=>'Ingrese Cantidad','required'])!!}
            </div><br/>
             <div class="form-grup">
               {!!Form::label('Precio unitario:')!!}
@@ -40,53 +40,7 @@
 		      {!!Form::submit('Guardar y Regresar',['class'=>'btn btn-primary'])!!}
 {!!form::close()!!}
 
-<!-- mostrar productos en tabla    -->
-  <table class="table table-hover" id="Productos-table">
-        <thead>
-            <tr>
-                <th>Id</th>
-				<th>Codigo</th>
-                <th>Nombre</th>
-                <th>Existencia</th>
-								
 
-            </tr>
-        </thead>
-    </table>
- 
-	
-@push('scripts')
-<script>
-	$('#Productos-table').DataTable({
-		processing: true,
-		serverSide: true,
-        ajax: '{!! route('datatables.data') !!}',
-		 columns: [
-            { data: 'id', name: 'id' },
-			{ data: 'codigo', name: 'codigo' },
-            { data: 'nombreProducto', name: 'nombreProducto' },
-            { data: 'existencia', name: 'existencia' },
-		        ],
-			"initComplete": function () {
-		     var api = this.api();
-            api.$('td').click( function () {
-		    api.search( this.innerHTML ).draw();
-				
-			
-            } );
-		},
-		"fnDrawCallback": function () {
-		     var api = this.api();
-            api.$('td').click( function () {
-			api.search( this.innerHTML ).draw();
-			//alert("el prod es : "+this.innerHTML);
-            } );
-		}
-		
-    });
-		
-</script>
-@endpush
 @stop
 
 <!-- mostrar productos en tabla    -->
